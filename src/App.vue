70d6cd61e8a2537d67b62199a3cbf24c5826973a
SalimKobi <salimkobi9@gmail.com>
2022-03-13 20:36:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="bg-amber-700">
+    <section>
+      <div class="pt-52">
+        <div
+          class="bg-white w-auto md:w-96 container mx-4 md:mx-auto rounded-2xl"
+        >
+          <img
+            class="p-5 rounded-3xl w-full md:w-auto"
+            src="./assets/img/image-qr-code.png"
+            alt=""
+          />
+          <div>
+            <h2 class="font-bold text-xl text-center pb-2 px-16">
+              Improve your front-end skills by building projects
+            </h2>
+            <p class="text-center px-14 pt-2 pb-11">
+              Scan the QR code to visit Frontend Mentors and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+body {
+}
+section {
+  min-height: 100vh;
+}
+</style>
